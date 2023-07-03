b6c08b0125fa3b17afc898c47f7d64ebfc203ec0
ANDRnewHuman <blsckcollapse@gmail.com>
2023-07-03 16:37:14 +0300

    1

---
+++ src/redux/contacts/contacts-reducer.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
+import actions from './contacts-actions';
+
+// console.log(actions.addContact.type);
+
+const items = createReducer([], {
+  //actions.addContact.type вычисляемые свойства объекта(приведётся к строке и подставится свойство type )
+  [actions.addContact]: (state, { payload }) => [...state, payload],
+  [actions.deleteContact]: (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
+
+const filter = createReducer('', {
+  [actions.changeFilter]: (_, { payload }) => payload,
+});
+
+export default combineReducers({
+  items,
+  filter,
+});
